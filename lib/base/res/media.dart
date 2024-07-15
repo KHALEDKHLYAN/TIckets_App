312f6957e4f57979d2f8900e74d2e928b6cd0540
KHALEDKHLYAN <khaledkhlyan@gmail.com>
2024-07-15 22:01:09 +0300

    hotels update

--- lib/base/res/media.dart
+++ lib/base/res/media.dart
@@ -1,4 +1,5 @@
 class AppMedia{
   static const _baseImage = "assets/images";
   static const logo = '$_baseImage/logo.png';
+  static const hotelRoom = '$_baseImage/hotel_room.png';
 }
--- lib/screens/widgets/hotels.dart
+++ lib/screens/widgets/hotels.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:ticket_app/base/res/media.dart';
+import 'package:ticket_app/base/res/styles/app_styles.dart';
 
 class Hotels extends StatelessWidget {
   const Hotels({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final size = MediaQuery.of(context).size;
+    return Container(
+      width: size.width * 0.6,
+      height: 350,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(24),
+        color: AppStyles.primaryColor,
+      ),
+      child: Column(children: [
+        Container(
+          height: 180,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(12),
+            // color: AppStyles.primaryColor,
+            image: DecorationImage(
+              image: AssetImage(AppMedia.hotelRoom)
+            ),
+          ),
+        ),
+      ]),
+    );
   }
 }
